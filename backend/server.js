e7b4481b42164af9572b43e4c02d028af2bf6e8c
nshoredev <nshoredev@gmail.com>
2025-03-08 05:12:56 -0500

    Notification routes and controllers added

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import rateLimit from 'express-rate-limit';
 import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
 import postRoutes from './routes/post.route.js';
+import notificationRoutes from './routes/notification.route.js';
 
 // utility functions
 import connectMongoDB from './db/connectMongoDB.js';
@@ -43,6 +44,7 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
+app.use('/api/notifications', notificationRoutes);
 
 app.listen(PORT, () => {
   console.log(`America is running on port ${PORT}`);
